5a1a2082c2f1f70243378ad2045c6f4df3960d74
abigmiu <suraelax@gmail.com>
2023-11-30 23:54:21 +0800

    会议室预订系统：统计管理模块-后端开发

--- nestjs/projects/meeting-room/server/src/app.module.ts
+++ nestjs/projects/meeting-room/server/src/app.module.ts
@@ -17,6 +17,7 @@ import { MeetingRoomModule } from './meeting-room/meeting-room.module';
 import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
 import { BookingModule } from './booking/booking.module';
 import { Booking } from './booking/entities/booking.entity';
+import { StatisticModule } from './statistic/statistic.module';
 
 @Module({
     imports: [
@@ -57,6 +58,7 @@ import { Booking } from './booking/entities/booking.entity';
         EmailModule,
         MeetingRoomModule,
         BookingModule,
+        StatisticModule,
     ],
     controllers: [AppController],
     providers: [
